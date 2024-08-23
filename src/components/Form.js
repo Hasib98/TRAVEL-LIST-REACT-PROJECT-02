379e1a3015b08bb6768bf92a603da8df5e3d4e64
Hasib98 <phoenixoffire1998@gmail.com>
2024-08-23 15:27:31 +0600

    FINISHD PROJECT| ADDED CLEAR ITEM FEATURE| SEPARATED COMPONENT FILES

---
+++ src/components/Form.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function hanndleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    // console.log(e);
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    // console.log(newItem);
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+  return (
+    <form className="add-form" onSubmit={hanndleSubmit}>
+      <h3>
+        ( This site is under Developemt ) 😋 What do you need for your 😍 trip
+      </h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+        {/* <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option> */}
+      </select>
+      <input
+        type="text"
+        placeholder="item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
